77d436337b8c44950ee5b60ab0423b27fa8479f4
Anam Rajdhami <anamrajdhami2000@gmail.com>
2023-03-09 14:54:34 +0545

    initial commit

---
+++ pattern1/Main.java
@@ -0,0 +1,11 @@
+public class Main{
+public static void main(String[] args){
+    for(int i=0; i<50; i++)
+    {
+        for( int j=0; j<100; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+}
+}
---
+++ pattern4/Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+public static void main(String[] args)
+    {
+        //first part begin
+    for(int i=0; i<5; i++)
+        {
+            for( int j=0; j<=i; j++)
+            {            
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        //first part end
+            //second part begin
+            for(int i=5; i>0; i--)
+        {
+            for( int j=1; j<=i; j++)
+            {            
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+            //second part end        
+    }
+}
+
+    
